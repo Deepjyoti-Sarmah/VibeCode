ae54f54dadab25374e2933511acdd0c745f3328d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-10-17 16:13:54 +0530

    09-message ui

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -1,26 +1,29 @@
 import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { z } from "zod"
+import { z } from "zod";
 
 export const messagesRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
         projectId: z.string().min(1, {
-          message: "projectId is required"
+          message: "projectId is required",
         }),
       }),
     )
     .query(async ({ input }) => {
       const messages = await prisma.message.findMany({
         where: {
-          projectId: input.projectId
+          projectId: input.projectId,
+        },
+        include: {
+          fragment: true,
         },
         orderBy: {
-          updatedAt: "desc"
+          updatedAt: "asc",
         },
-      })
+      });
 
       return messages;
     }),
@@ -28,15 +31,17 @@ export const messagesRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
-        value: z.string().min(1, {
-          message: "Value is required"
-        })
+        value: z
+          .string()
+          .min(1, {
+            message: "Value is required",
+          })
           .max(10000, {
-            message: "Value is too long"
+            message: "Value is too long",
           }),
 
         projectId: z.string().min(1, {
-          message: "projectId is required"
+          message: "projectId is required",
         }),
       }),
     )
@@ -47,7 +52,7 @@ export const messagesRouter = createTRPCRouter({
           content: input.value,
           role: "USER",
           type: "RESULT",
-        }
+        },
       });
 
       await inngest.send({
@@ -55,9 +60,9 @@ export const messagesRouter = createTRPCRouter({
         data: {
           value: input.value,
           projectId: input.projectId,
-        }
+        },
       });
 
       return createdMessage;
-    })
-})
+    }),
+});
